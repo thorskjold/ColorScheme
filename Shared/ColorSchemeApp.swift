31cbe892bd5d9a74c031ca98b31b6d1d36d84a21
Thor <thorlindberg@icloud.com>
2021-02-27 23:00:32 +0100

    Initial Commit

---
+++ Shared/ColorSchemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSchemeApp.swift
+//  Shared
+//
+//  Created by Thor Lindberg on 27/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorSchemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
